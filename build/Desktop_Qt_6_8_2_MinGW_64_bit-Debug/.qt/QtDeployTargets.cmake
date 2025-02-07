f3dbac812dbe4eceef753bf6fce8b8dbff2c4663
Somi <152510800+SomiSrbija@users.noreply.github.com>
2025-02-07 10:02:20 +0100

    Delete build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_LumePad_FILE C:/Users/somi/Documents/LumePad/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/LumePad.exe)
-set(__QT_DEPLOY_TARGET_LumePad_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_LumePad_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
