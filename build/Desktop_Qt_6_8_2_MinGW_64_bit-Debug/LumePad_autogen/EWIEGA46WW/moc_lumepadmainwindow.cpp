79c49a2a339ff70a64ca92b2e713e04244d9b500
SomiSrbija <milivk060919@student.jenseneducation.se>
2025-02-07 09:35:39 +0100

    Initial commit

---
+++ build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/LumePad_autogen/EWIEGA46WW/moc_lumepadmainwindow.cpp
@@ -0,0 +1,224 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'lumepadmainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.8.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../lumepadmainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'lumepadmainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.8.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN17LumePadMainWindowE_t {};
+} // unnamed namespace
+
+
+#ifdef QT_MOC_HAS_STRINGDATA
+static constexpr auto qt_meta_stringdata_ZN17LumePadMainWindowE = QtMocHelpers::stringData(
+    "LumePadMainWindow",
+    "newFile",
+    "",
+    "openFile",
+    "saveFile",
+    "saveFileAs",
+    "closeCurrentTab",
+    "documentModified",
+    "findText",
+    "replaceText",
+    "about",
+    "goToLine",
+    "zoomIn",
+    "zoomOut",
+    "toggleWordWrap",
+    "changeFont",
+    "toggleLineNumbers",
+    "toggleFullScreen",
+    "openRecentFile"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+
+Q_CONSTINIT static const uint qt_meta_data_ZN17LumePadMainWindowE[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      17,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  116,    2, 0x08,    1 /* Private */,
+       3,    0,  117,    2, 0x08,    2 /* Private */,
+       4,    0,  118,    2, 0x08,    3 /* Private */,
+       5,    0,  119,    2, 0x08,    4 /* Private */,
+       6,    0,  120,    2, 0x08,    5 /* Private */,
+       7,    0,  121,    2, 0x08,    6 /* Private */,
+       8,    0,  122,    2, 0x08,    7 /* Private */,
+       9,    0,  123,    2, 0x08,    8 /* Private */,
+      10,    0,  124,    2, 0x08,    9 /* Private */,
+      11,    0,  125,    2, 0x08,   10 /* Private */,
+      12,    0,  126,    2, 0x08,   11 /* Private */,
+      13,    0,  127,    2, 0x08,   12 /* Private */,
+      14,    0,  128,    2, 0x08,   13 /* Private */,
+      15,    0,  129,    2, 0x08,   14 /* Private */,
+      16,    0,  130,    2, 0x08,   15 /* Private */,
+      17,    0,  131,    2, 0x08,   16 /* Private */,
+      18,    0,  132,    2, 0x08,   17 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject LumePadMainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_ZN17LumePadMainWindowE.offsetsAndSizes,
+    qt_meta_data_ZN17LumePadMainWindowE,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_tag_ZN17LumePadMainWindowE_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<LumePadMainWindow, std::true_type>,
+        // method 'newFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'openFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'saveFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'saveFileAs'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'closeCurrentTab'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'documentModified'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'findText'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'replaceText'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'about'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'goToLine'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'zoomIn'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'zoomOut'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'toggleWordWrap'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'changeFont'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'toggleLineNumbers'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'toggleFullScreen'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'openRecentFile'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void LumePadMainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<LumePadMainWindow *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->newFile(); break;
+        case 1: _t->openFile(); break;
+        case 2: _t->saveFile(); break;
+        case 3: _t->saveFileAs(); break;
+        case 4: _t->closeCurrentTab(); break;
+        case 5: _t->documentModified(); break;
+        case 6: _t->findText(); break;
+        case 7: _t->replaceText(); break;
+        case 8: _t->about(); break;
+        case 9: _t->goToLine(); break;
+        case 10: _t->zoomIn(); break;
+        case 11: _t->zoomOut(); break;
+        case 12: _t->toggleWordWrap(); break;
+        case 13: _t->changeFont(); break;
+        case 14: _t->toggleLineNumbers(); break;
+        case 15: _t->toggleFullScreen(); break;
+        case 16: _t->openRecentFile(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *LumePadMainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *LumePadMainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ZN17LumePadMainWindowE.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int LumePadMainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 17)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 17;
+    }
+    return _id;
+}
+QT_WARNING_POP
